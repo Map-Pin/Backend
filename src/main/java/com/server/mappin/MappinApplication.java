d3b5049ba962f7395154dfa327a944f2729e0fe3
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-31 17:16:55 +0900

    init

---
+++ src/main/java/com/server/mappin/MappinApplication.java
@@ -0,0 +1,13 @@
+package com.server.mappin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappinApplication.class, args);
+	}
+
+}
