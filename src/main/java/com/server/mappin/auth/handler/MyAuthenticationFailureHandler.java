578e30c97adba329924a18eb1e8d609e75d4b3b9
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-31 19:47:27 +0900

    Update: Failure Handler

---
+++ src/main/java/com/server/mappin/auth/handler/MyAuthenticationFailureHandler.java
@@ -0,0 +1,24 @@
+package com.server.mappin.auth.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class MyAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException,
+            ServletException {
+        // 인증 실패시 메인 페이지로 이동
+        log.error("ACCESS DENIED");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+
+}
