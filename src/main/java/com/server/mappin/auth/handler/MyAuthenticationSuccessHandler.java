a3c7fabf73cc843073a22ffc546dc7e6bd47317c
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-10-31 19:46:11 +0900

    Update: Handler

--- src/main/java/com/server/mappin/auth/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/server/mappin/auth/handler/MyAuthenticationSuccessHandler.java
@@ -59,7 +59,6 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
 
 
         } else {
-
             // 회원이 존재하지 않을경우, 서비스 제공자와 email을 쿼리스트링으로 전달하는 url을 만들어준다.
             String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/loginSuccess")
                     .queryParam("email", (String) oAuth2User.getAttribute("email"))
