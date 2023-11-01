95efd766f263d51b2e2b0c92ae88055119516811
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 22:31:06 +0900

    Update: 로그인 메서드

--- src/main/java/com/server/mappin/service/MemberService.java
+++ src/main/java/com/server/mappin/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.server.mappin.service;
 
+import com.server.mappin.auth.token.TokenProvider;
 import com.server.mappin.domain.Member;
+import com.server.mappin.dto.LoginResponseDto;
+import com.server.mappin.dto.MemberLoginDto;
 import com.server.mappin.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,24 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
-
+    private final TokenProvider tokenProvider;
 
     @Transactional
     public Optional<Member> findByEmail(String email) {
         return memberRepository.findByEmail(email);
+    }
+    @Transactional
+    public LoginResponseDto create(MemberLoginDto memberCreateDto) {
+        Member member = new Member();
+        member.setEmail(memberCreateDto.getEmail());
+        member.setRole(memberCreateDto.getRole());
+        String jwt = tokenProvider.generateToken(memberCreateDto.getEmail(), memberCreateDto.getRole().toString());
+        Member save = memberRepository.save(member);
+
+        return LoginResponseDto.builder()
+                .id(save.getId())
+                .jwt(jwt)
+                .build();
+
     }
 }
