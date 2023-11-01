a021e70cd658d114bb08326a7a69016ab2667bd5
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 14:36:57 +0900

    Update: 메서드 수정중

--- src/main/java/com/server/mappin/auth/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/server/mappin/auth/handler/MyAuthenticationSuccessHandler.java
@@ -1,15 +1,19 @@
 package com.server.mappin.auth.handler;
 
+import com.server.mappin.auth.oauth.ProviderType;
 import com.server.mappin.auth.token.TokenDto;
 import com.server.mappin.auth.token.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,7 +36,6 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
         String email = oAuth2User.getAttribute("email");
         // 서비스 제공 플랫폼(GOOGLE, KAKAO, NAVER)이 어디인지 가져온다.
         String provider = oAuth2User.getAttribute("provider");
-
         // CustomOAuth2UserService에서 셋팅한 로그인한 회원 존재 여부를 가져온다.
         boolean isExist = oAuth2User.getAttribute("exist");
         // OAuth2User로 부터 Role을 얻어온다.
@@ -40,7 +43,6 @@ public class MyAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucce
                 findFirst() // 첫번째 Role을 찾아온다.
                 .orElseThrow(IllegalAccessError::new) // 존재하지 않을 시 예외를 던진다.
                 .getAuthority(); // Role을 가져온다.
-
         // 회원이 존재할경우
         if (isExist) {
             // 회원이 존재하면 jwt token 발행을 시작한다.
