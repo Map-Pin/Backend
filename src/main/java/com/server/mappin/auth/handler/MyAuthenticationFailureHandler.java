2f6073bfdd311bc417bbd6fc81b34022eb559ed0
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 14:32:43 +0900

    Update: log 메세지 추가

--- src/main/java/com/server/mappin/auth/handler/MyAuthenticationFailureHandler.java
+++ src/main/java/com/server/mappin/auth/handler/MyAuthenticationFailureHandler.java
@@ -17,7 +17,7 @@ public class MyAuthenticationFailureHandler implements AuthenticationFailureHand
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException,
             ServletException {
         // 인증 실패시 메인 페이지로 이동
-        log.error("ACCESS DENIED");
+        log.error(exception.getMessage());
         response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 
