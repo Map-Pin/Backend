bd26b9f873397140014843ddfe8ec5ff921c63b2
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 14:35:47 +0900

    Update: 로그인 엔드포인트 변경

--- src/main/java/com/server/mappin/controller/MemberController.java
+++ src/main/java/com/server/mappin/controller/MemberController.java
@@ -2,18 +2,24 @@ package com.server.mappin.controller;
 
 import com.server.mappin.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
-
-    @GetMapping("/auth/kakao/callback")
-    public ResponseEntity<?> join(@RequestParam(value = "code") String code){
+    @GetMapping("/loginSuccess")
+    public ResponseEntity<?> join(@RequestParam(value = "email") String email,
+                                  @RequestParam(value = "provider") String provider){
+        System.out.println("email = " + email);
+        System.out.println("provider = " + provider);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
