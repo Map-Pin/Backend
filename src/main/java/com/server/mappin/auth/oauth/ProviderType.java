1cf0e570bae7872ef1b4ec88ec0b0cf2fa834d14
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 14:33:57 +0900

    Update: toString 메서드 추가

--- src/main/java/com/server/mappin/auth/oauth/ProviderType.java
+++ src/main/java/com/server/mappin/auth/oauth/ProviderType.java
@@ -1,5 +1,17 @@
 package com.server.mappin.auth.oauth;
 
 public enum ProviderType {
-    KAKAO, GOOGLE
+    KAKAO, GOOGLE;
+
+    @Override
+    public String toString(){
+        switch (this){
+            case KAKAO:
+                return "Kakao";
+            case GOOGLE:
+                    return "Google";
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
 }
