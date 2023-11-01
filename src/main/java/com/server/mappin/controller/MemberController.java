90df7ea3417579a3d36adef78d65a769921e6455
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-01 21:46:00 +0900

    Update: 소셜 로그인 메서드 수정

--- src/main/java/com/server/mappin/controller/MemberController.java
+++ src/main/java/com/server/mappin/controller/MemberController.java
@@ -3,11 +3,8 @@ package com.server.mappin.controller;
 import com.server.mappin.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,11 +12,6 @@ import java.util.logging.Logger;
 public class MemberController {
 
     private final MemberService memberService;
-    @GetMapping("/loginSuccess")
-    public ResponseEntity<?> join(@RequestParam(value = "email") String email,
-                                  @RequestParam(value = "provider") String provider){
-        System.out.println("email = " + email);
-        System.out.println("provider = " + provider);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+
+
 }
--- src/main/java/com/server/mappin/repository/MemberRepository.java
+++ src/main/java/com/server/mappin/repository/MemberRepository.java
@@ -4,10 +4,15 @@ import com.server.mappin.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
     Optional<Member> findByEmail(String email);
+
+    @Override
+    <S extends Member> S save(S entity);
+
 }
