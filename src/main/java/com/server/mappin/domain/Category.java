c3eafce097826ea1117cd7f39b50aea3a7904946
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-02 16:58:26 +0900

    Update: @Getter 추가

--- src/main/java/com/server/mappin/domain/Category.java
+++ src/main/java/com/server/mappin/domain/Category.java
@@ -1,11 +1,15 @@
 package com.server.mappin.domain;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "category")
+@Getter
+@NoArgsConstructor
 public class Category {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,7 +19,10 @@ public class Category {
   @Column(name = "name")
   private String name;
 
-
-
+  @Builder
+  public Category(Long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
