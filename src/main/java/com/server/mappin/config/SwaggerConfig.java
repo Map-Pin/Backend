633ea4dbd30c55f5dd68c8e30825883cfc31b732
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-02 14:22:17 +0900

    Update: swagger jwt 토큰 인증 추가

--- src/main/java/com/server/mappin/config/SwaggerConfig.java
+++ src/main/java/com/server/mappin/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.server.mappin.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,20 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI(){
+        String jwtSchemaName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemaName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemaName, new SecurityScheme()
+                        .name(jwtSchemaName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info());
+                .info(info())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+
     }
 
     private Info info(){
