0b1516a684d0a875fae1a631f264eaf47dd51067
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-02 16:59:22 +0900

    Update: Getter 추가

--- src/main/java/com/server/mappin/domain/Location.java
+++ src/main/java/com/server/mappin/domain/Location.java
@@ -1,12 +1,15 @@
 package com.server.mappin.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "location")
+@NoArgsConstructor
 public class Location {
 
   @Id
@@ -16,5 +19,10 @@ public class Location {
 
   @Column(name = "dong")
   private String dong;
+  @Builder
+  public Location(Long id, String dong) {
+    this.id = id;
+    this.dong = dong;
+  }
 }
 
