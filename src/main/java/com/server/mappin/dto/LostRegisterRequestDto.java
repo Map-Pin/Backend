5b7d7f7aebbb8f6e27d0a92cf2228d63b9e0a3d8
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-06 19:23:58 +0900

    Update: RequestDto 수정

--- src/main/java/com/server/mappin/dto/LostRegisterRequestDto.java
+++ src/main/java/com/server/mappin/dto/LostRegisterRequestDto.java
@@ -1,10 +1,12 @@
 package com.server.mappin.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.server.mappin.domain.Category;
 import com.server.mappin.domain.Location;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.geo.Point;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
@@ -12,7 +14,8 @@ import java.time.LocalDate;
 public class LostRegisterRequestDto {
     private String title;
     private String content;
-    private LocalDate foundDate;
+    private String foundDate;
+    private MultipartFile image;
     private Double x;
     private Double y;
     private String dong;
