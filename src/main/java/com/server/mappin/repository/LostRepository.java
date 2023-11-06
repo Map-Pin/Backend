d325b24eb3fe83cf5ad6164b879874306cf0077e
SunwuPark <sunwu5678@gmail.com>
2023-11-07 00:05:50 +0900

    [#43] Update: LostRepository.java

--- src/main/java/com/server/mappin/repository/LostRepository.java
+++ src/main/java/com/server/mappin/repository/LostRepository.java
@@ -17,6 +17,9 @@ public interface LostRepository extends JpaRepository<Lost, Long> {
 
     @Query(value = "select l from Lost l left join fetch l.location c where c.dong = :dong")
     List<Lost> findLocationByDong(@Param("dong") String dong);
+
+    @Query("select l from Lost l " + "where l.member.id in (select s.member.id from Shop s where s.name = :name)")
+    List<Lost> findLostByShopName(@Param("name") String name);
     @Override
     <S extends Lost> S save(S entity);
 }
