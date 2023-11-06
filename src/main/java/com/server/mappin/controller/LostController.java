39abe891b6fdfa9f025de219bd43f1fb5932bbba
SunwuPark <sunwu5678@gmail.com>
2023-11-06 16:30:46 +0900

    [#38] Add: Lost Controller

--- src/main/java/com/server/mappin/controller/LostController.java
+++ src/main/java/com/server/mappin/controller/LostController.java
@@ -1,6 +1,9 @@
 package com.server.mappin.controller;
 
-import com.server.mappin.dto.*;
+import com.server.mappin.dto.FindByCategoryResponseDto;
+import com.server.mappin.dto.FindByDongResponseDto;
+import com.server.mappin.dto.LostRegisterRequestDto;
+import com.server.mappin.dto.LostRegisterResponseDto;
 import com.server.mappin.service.LostService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -25,8 +28,7 @@ public class LostController {
     private final LostService lostService;
 
     @Operation(summary = "분실물 등록")
-    @ApiResponse(responseCode ="200",description ="분실물 등록 성공",content = @Content(schema = @Schema(implementation = LostRegisterResponseDto.class)))
-    @ApiResponse(responseCode ="400",description ="분실물 등록 실패",content = @Content(schema = @Schema(implementation = LostRegisterResponseDto.class)))
+    @ApiResponse(content = @Content(schema = @Schema(implementation = LostRegisterResponseDto.class)))
     @PutMapping("/lost/register")
     public ResponseEntity<?> registerLost(@RequestBody LostRegisterRequestDto lostRegisterRequestDto, Authentication authentication){
         try{
@@ -38,12 +40,24 @@ public class LostController {
     }
 
     @Operation(summary = "카테고리 검색",description = "분실물을 카테고리 별로 검색")
-    @ApiResponse(responseCode = "200",description ="카테고리 별로 검색 성공", content = @Content(schema = @Schema(implementation = FindByCategoryListResponseDto.class)))
+    @ApiResponse(content = @Content(schema = @Schema(implementation = FindByCategoryResponseDto.class)))
     @GetMapping("/search/category/{category_name}")
     public ResponseEntity<?> searchByCategory(@RequestParam(value = "category_name") String name){
         try{
-            FindByCategoryListResponseDto findByCategoryListResponseDto = lostService.findByCategory(name);
-            return new ResponseEntity<>(findByCategoryListResponseDto,HttpStatus.OK);
+            List<FindByCategoryResponseDto> byCategory = lostService.findByCategory(name);
+            return new ResponseEntity<>(byCategory,HttpStatus.OK);
+        }catch (IllegalStateException e){
+            return new ResponseEntity<>("에러가 발생했습니다", HttpStatus.CONFLICT);
+        }
+    }
+
+    @Operation(summary = "동 검색",description = "분실물을 동 별로 검색")
+    @ApiResponse(content = @Content(schema = @Schema(implementation = FindByDongResponseDto.class)))
+    @GetMapping("/search/dong/{dong_name}")
+    public ResponseEntity<?> searchByDong(@RequestParam(value = "dong_name") String dongName){
+        try{
+            List<FindByDongResponseDto> byDong = lostService.findByDong(dongName);
+            return new ResponseEntity<>(byDong,HttpStatus.OK);
         }catch (IllegalStateException e){
             return new ResponseEntity<>("에러가 발생했습니다", HttpStatus.CONFLICT);
         }
