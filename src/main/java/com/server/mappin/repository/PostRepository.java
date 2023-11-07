e5332d6ee178c9ef0a6c4393d6c00641607dfc1c
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-07 16:13:23 +0900

    Update: 아이디로 조회

--- src/main/java/com/server/mappin/repository/PostRepository.java
+++ src/main/java/com/server/mappin/repository/PostRepository.java
@@ -5,9 +5,14 @@ import com.server.mappin.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Override
     <S extends Post> S save(S entity);
+
+    @Override
+    Optional<Post> findById(Long aLong);
 }
