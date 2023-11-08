e2725ae3c345696f9077eac84586c5b8d788f1f2
SunwuPark <sunwu5678@gmail.com>
2023-11-08 16:47:02 +0900

    [#63] Update: Add PostService Update

--- src/main/java/com/server/mappin/service/PostService.java
+++ src/main/java/com/server/mappin/service/PostService.java
@@ -4,9 +4,7 @@ import com.server.mappin.domain.Category;
 import com.server.mappin.domain.Location;
 import com.server.mappin.domain.Member;
 import com.server.mappin.domain.Post;
-import com.server.mappin.dto.PostCreateRequestDto;
-import com.server.mappin.dto.PostCreateResponseDto;
-import com.server.mappin.dto.PostSearchResponseDto;
+import com.server.mappin.dto.*;
 import com.server.mappin.repository.CategoryRepository;
 import com.server.mappin.repository.LocationRepository;
 import com.server.mappin.repository.MemberRepository;
@@ -21,7 +19,6 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -91,6 +88,85 @@ public class PostService {
             .isSuccess("false")
             .build();
   }
+
+  @Transactional
+  public PostUpdateResponseDto update(Long postId, PostUpdateRequestDto postUpdateRequestDto, MultipartFile image, String email) throws IOException {
+    Post post = postRepository.findById(postId).orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다"));
+
+    Optional<Member> member = memberRepository.findByEmail(email);
+    if(member.isEmpty()){
+      return PostUpdateResponseDto.builder()
+              .statusCode(400)
+              .isSuccess("false")
+              .build();
+    }
+    else if(!member.get().getId().equals(post.getMember().getId())){
+      return PostUpdateResponseDto.builder()
+              .statusCode(400)
+              .isSuccess("false")
+              .build();
+    }
+
+    // 업데이트 필드를 확인하고 필요한 경우 업데이트
+    if (postUpdateRequestDto.getLostDate() != null) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      LocalDate localDate = LocalDate.parse(postUpdateRequestDto.getLostDate(), formatter);
+      post.setLostDate(localDate);
+    }
+
+    if (postUpdateRequestDto.getX() != null && postUpdateRequestDto.getY() != null) {
+      String dong = mapService.getDong(postUpdateRequestDto.getX(), postUpdateRequestDto.getY());
+      Optional<Location> locationByDong = locationRepository.findLocationByDong(dong);
+      locationByDong.ifPresent(post::setLocation);
+    }
+
+    if (postUpdateRequestDto.getCategory() != null) {
+      Optional<Category> categoryByName = categoryRepository.findCategoryByName(postUpdateRequestDto.getCategory());
+      categoryByName.ifPresent(post::setCategory);
+    }
+
+    if (postUpdateRequestDto.getContent() != null) {
+      post.setContent(postUpdateRequestDto.getContent());
+    }
+
+    if (postUpdateRequestDto.getTitle() != null) {
+      post.setTitle(postUpdateRequestDto.getTitle());
+    }
+
+    if (image != null) {
+      // 이미지 업로드 및 기존 이미지 삭제 처리
+      if (!s3Service.findByUrl(post.getImageUrl()).isEmpty()) {
+        s3Service.delete(post.getImageUrl());
+      }
+      String imageUrl = s3Service.upload(image, "images");
+      post.setImageUrl(imageUrl);
+    }
+
+    // 게시물 업데이트
+    Post updatedPost = postRepository.save(post);
+
+    if (updatedPost != null) {
+      return PostUpdateResponseDto.builder()
+              .statusCode(200)
+              .isSuccess("true")
+              .postId(updatedPost.getId())
+              .image(updatedPost.getImageUrl())
+              .createdAt(updatedPost.getCreatedAt())
+              .content(updatedPost.getContent())
+              .x(updatedPost.getX())
+              .y(updatedPost.getY())
+              .dong(updatedPost.getLocation().getDong())
+              .category(updatedPost.getCategory().getName())
+              .lostDate(updatedPost.getLostDate())
+              .build();
+    } else {
+      return PostUpdateResponseDto.builder()
+              .statusCode(400)
+              .isSuccess("false")
+              .build();
+    }
+  }
+
 
 
   public PostSearchResponseDto search(Long id){
