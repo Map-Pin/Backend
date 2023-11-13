274ceec2a7120d17f2fcc9e52c7d138565a6a9fe
SunwuPark <sunwu5678@gmail.com>
2023-11-13 20:33:00 +0900

    UPDATE: MemberService created

--- src/main/java/com/server/mappin/service/MemberService.java
+++ src/main/java/com/server/mappin/service/MemberService.java
@@ -5,21 +5,19 @@ import com.server.mappin.domain.Member;
 import com.server.mappin.domain.Shop;
 import com.server.mappin.domain.enums.ProviderType;
 import com.server.mappin.domain.enums.Role;
-import com.server.mappin.dto.AdminLoginResponseDto;
-import com.server.mappin.dto.LoginResponseDto;
-import com.server.mappin.dto.UserLoginResponseDto;
-import com.server.mappin.dto.MemberLoginDto;
+import com.server.mappin.dto.Login.AdminLoginResponseDto;
+import com.server.mappin.dto.Login.LoginResponseDto;
+import com.server.mappin.dto.Login.MemberLoginDto;
+import com.server.mappin.dto.Login.UserLoginResponseDto;
 import com.server.mappin.repository.MemberRepository;
 import com.server.mappin.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.geo.Point;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
