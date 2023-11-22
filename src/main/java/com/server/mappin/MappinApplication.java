8fcb6748e2232be267d4a0eedb6191cb25041fa5
SunwuPark <sunwu5678@gmail.com>
2023-11-22 14:27:34 +0900

    UPDATE: main 시간 설정 오타 수정

--- src/main/java/com/server/mappin/MappinApplication.java
+++ src/main/java/com/server/mappin/MappinApplication.java
@@ -3,9 +3,15 @@ package com.server.mappin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MappinApplication {
-
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MappinApplication.class, args);
 	}
