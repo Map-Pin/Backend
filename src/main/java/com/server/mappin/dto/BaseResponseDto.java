1bf9afda6ccdccf33deeddab60163cac01cf51b0
SunwuPark <sunwu5678@gmail.com>
2023-11-23 21:49:01 +0900

    UPDATE: BaseResponseDto update add json

--- src/main/java/com/server/mappin/dto/BaseResponseDto.java
+++ src/main/java/com/server/mappin/dto/BaseResponseDto.java
@@ -1,5 +1,6 @@
 package com.server.mappin.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,19 +15,20 @@ import lombok.RequiredArgsConstructor;
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 @Schema(description = "기본 응답")
 public class BaseResponseDto<T> {
-
   @JsonProperty("isSuccess")
-  private String isSuccess;
+  private Boolean isSuccess;
+  private String message;
   private Integer code;
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private T result;
 
   // 성공한 경우 응답 생성
   public static <T> BaseResponseDto<T> of(String message, Integer code, T data){
-    return new BaseResponseDto<>(message, code, data);
+    return new BaseResponseDto<>(true ,message, code, data);
   }
 
   // 실패한 경우 응답 생성
   public static <T> BaseResponseDto<T> onFailure(String message, Integer code, T data){
-    return new BaseResponseDto<>(message, code, data);
+    return new BaseResponseDto<>(false, message, code, data);
   }
 }
