090fb267f80092c797cfcde789d75ec6c8019cee
SunwuPark <sunwu5678@gmail.com>
2023-11-24 15:13:03 +0900

    ADD: ChatRoomMemberRepository.java

---
+++ src/main/java/com/server/mappin/repository/ChatRoomMemberRepository.java
@@ -0,0 +1,9 @@
+package com.server.mappin.repository;
+
+import com.server.mappin.domain.ChatRoomMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRoomMemberRepository extends JpaRepository<ChatRoomMember, Long> {
+}
