88aa8f85168df7978acb859db4ae4dd017aad233
SunwuPark <sunwu5678@gmail.com>
2023-11-24 15:14:13 +0900

    ADD: ChatRepository.java

---
+++ src/main/java/com/server/mappin/repository/ChatRepository.java
@@ -0,0 +1,9 @@
+package com.server.mappin.repository;
+
+import com.server.mappin.domain.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRepository extends JpaRepository<ChatMessage, Long> {
+}
