c38e2b8d005b5a0ae2cb587d15105c9b8f356820
SunwuPark <sunwu5678@gmail.com>
2023-11-24 15:12:22 +0900

    UPDATE: MessageController.java

--- src/main/java/com/server/mappin/controller/MessageController.java
+++ src/main/java/com/server/mappin/controller/MessageController.java
@@ -1,22 +1,26 @@
 package com.server.mappin.controller;
 
 import com.server.mappin.domain.ChatMessage;
+import com.server.mappin.dto.BaseResponseDto;
+import com.server.mappin.dto.ChatMessageDto;
+import com.server.mappin.repository.ChatRepository;
+import com.server.mappin.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class MessageController {
 
-  private final SimpMessageSendingOperations sendingOperations;
+  private final ChatService chatService;
 
   @MessageMapping("/chat/message")
-  public void enter(ChatMessage message) {
-    if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
-      message.setMessage(message.getSender()+"님이 입장하였습니다.");
-    }
-    sendingOperations.convertAndSend("/topic/chat/room/"+message.getRoomId(),message);
+  public BaseResponseDto<Object> enter(ChatMessageDto message, Authentication authentication) {
+    //System.out.println("AUTENNT: " + authentication.getName());
+    chatService.send(message);
+    return BaseResponseDto.of("success", 200, null);
   }
 }
