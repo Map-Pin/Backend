b0b8f807c9660623d029b3ad399d0cc420089c06
SunwuPark <sunwu5678@gmail.com>
2023-11-24 15:14:04 +0900

    ADD: ChatRoom.java

--- src/main/java/com/server/mappin/domain/ChatRoom.java
+++ src/main/java/com/server/mappin/domain/ChatRoom.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
@@ -14,13 +16,17 @@ import java.util.UUID;
 @Entity
 public class ChatRoom {
   @Id
+  @Column(name = "chatroom_id")
   private String roomId;
   private String roomName;
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
 
   public static ChatRoom create(String name) {
     ChatRoom room = new ChatRoom();
     room.roomId = UUID.randomUUID().toString();
     room.roomName = name;
+    room.createdAt = LocalDateTime.now();
     return room;
   }
 }
