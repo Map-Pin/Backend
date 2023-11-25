dc57ef5680d2e0a79020451fc571b9e69e11ec16
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-11-25 19:07:35 +0900

    Add: 사용자 지정 예외

---
+++ src/main/java/com/server/mappin/exception/GeneralException.java
@@ -0,0 +1,18 @@
+package com.server.mappin.exception;
+
+import com.server.mappin.common.BaseErrorCode;
+import com.server.mappin.common.BaseResponseDto;
+import com.server.mappin.common.ErrorReasonDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+    private final BaseErrorCode baseErrorCode;
+
+    public ErrorReasonDto getReason(){
+        return this.baseErrorCode.getReason();
+    }
+
+}
