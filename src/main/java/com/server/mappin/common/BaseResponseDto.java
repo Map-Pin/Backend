fb87970dd7b56226f89362da1ceb22435efc5d48
SunwuPark <sunwu5678@gmail.com>
2023-11-26 16:52:12 +0900

    UPDATE: isSuccess -> success

--- src/main/java/com/server/mappin/common/BaseResponseDto.java
+++ src/main/java/com/server/mappin/common/BaseResponseDto.java
@@ -1,7 +1,6 @@
 package com.server.mappin.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.server.mappin.common.status.SuccessStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,11 +9,10 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+@JsonPropertyOrder({"success", "code", "message", "result"})
 @Schema(description = "기본 응답")
 public class BaseResponseDto<T> {
-  @JsonProperty("isSuccess")
-  private final boolean isSuccess;
+  private final boolean success;
   private final String code;
   private final String message;
   @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -29,7 +27,7 @@ public class BaseResponseDto<T> {
   }
 
   // 실패한 경우 응답 생성
-  public static <T> BaseResponseDto<T> onFailure(String message, String code, T data){
-    return new BaseResponseDto<>(false,code,message, data);
+  public static <T> BaseResponseDto<T> onFailure(String message, String code, T data) {
+    return new BaseResponseDto<>(false, code, message, data);
   }
 }
