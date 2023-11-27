a300235c68a4291e30cf52318e79f1cb6cbab31f
SunwuPark <sunwu5678@gmail.com>
2023-11-28 01:35:59 +0900

    UPDATE: LostService.java

--- src/main/java/com/server/mappin/service/LostService.java
+++ src/main/java/com/server/mappin/service/LostService.java
@@ -11,7 +11,14 @@ import java.io.IOException;
 public interface LostService {
   CategoryDTO.CategoryListRP findByCategory(String categoryName);
 
+  LocationDTO.LocationListRP findByCurrentLocation(Double x, Double y);
+
+  LostDTO.LostRegisterRP registerLost(LostDTO.LostRegisterRQ lostRegisterRQ, MultipartFile image, String email) throws IOException;
   LocationDTO.LocationListRP findByDong(String dongName);
+
+  LostDTO.LostSearchByIdRP getById(Long id);
+
+  LostDTO.LostUpdateRP update(Long lostId, LostDTO.LostUpdateRQ lostUpdateRQ, MultipartFile image, String email) throws IOException;
 
   ShopDTO.ShopListRP findByShop(String shopName);
 }
